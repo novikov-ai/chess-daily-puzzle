50bcfce49aa1958c5207eec8d69445fb0397d413
ainovikov <ainovikov@avito.ru>
2023-09-09 21:53:06 +0300

    add fetcher

---
+++ internal/usecases/fetching/fetch.go
@@ -0,0 +1,32 @@
+package fetching
+
+import (
+	"chess-daily-puzzle/internal/models"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+const apiPuzzleDaily = "https://lichess.org/api/puzzle/daily"
+
+func DailyPuzzle() (models.Puzzle, error) {
+	resp, err := http.Get(apiPuzzleDaily)
+	if err != nil {
+		return models.Puzzle{}, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return models.Puzzle{}, err
+	}
+
+	var puzzle models.Puzzle
+	err = json.Unmarshal(body, &puzzle)
+
+	if err != nil {
+		return models.Puzzle{}, err
+	}
+
+	return puzzle, nil
+}
