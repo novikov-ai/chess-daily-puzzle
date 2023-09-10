d8b69381b26566e9a400fba5549f00474931a6f9
ainovikov <ainovikov@avito.ru>
2023-09-11 00:45:50 +0300

    rename

--- internal/usecases/presentation/config.go
+++ internal/usecases/presentation/config.go
@@ -4,8 +4,8 @@ const (
 	// Username - Bot name
 	Username = "Daily Puzzle"
 
-	// TextAttr - %s - required placeholder for gameURL
-	TextAttr = "Перерыв на головоломку!\n\nСможете [найти](%s) лучшее продолжение в партии?"
+	// Message - %s - required placeholder for gameURL
+	Message = "Ready to test your chess wizardry?\n\n[Dive](%s) into today's Brain-Teaser!"
 
 	// IconURL - Bot avatar
 	IconURL = "https://lichess1.org/assets/_44IzGj/logo/lichess-favicon-128.png"
